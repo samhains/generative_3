1d9247d9e515e8be5117d97de49be09df52b085e
Sam Hains <sdh@eml.cc>
2016-02-13 03:35:06 +1100

    added a second worm

--- empty-example/worm2.js
+++ empty-example/worm2.js
@@ -13,22 +13,13 @@ var yoff = 0;
 var worm;
 var worm2;
 
-function setup() {
-  createCanvas(640, 360);
-  // Initialize all values
-  r = height * 1.45;
-	worm = new Worm(0.02);
-	worm2 = new Worm2(0.02);
-	// worm2 = new Worm(0.04);
-	// we need to draw lines in between the dots
-	// worm2 = new Worm(2.04);
-}
 
 function Trail(x, y) {
 	this.x = x;
 	this.y = y;
 }
-function Worm(theta){
+
+function Worm2(theta){
 	this.theta = theta;
 	this.trails = []
 	this.display = function(){
@@ -40,29 +31,13 @@ function Worm(theta){
 		strokeWeight(2);
 		// line(trail.x, trail.y, 1, 1);
 		this.trails.forEach(function(trail){
-			stroke(random(50), random(250), random(50));
+			stroke(random(50), random(50), random(250));
 			ellipse(trail.x, trail.y, 1, 1);
 			line(4/trail.x, trail.y, 1, 1);
 		});
 		// ellipse(x, y, 1, 1);
 		this.trails.push(new Trail(x, y));
-		this.theta += this.theta*0.0000995*x;
+		this.theta += this.theta*0.000995*x;
 	};
 }
 
-function draw() {
-  //background(random(100));
-
-  // Translate the origin point to the center of the screen
-  translate(width/2, height/2);
-
-	xoff = xoff + 0.01;
-	yoff = yoff - 0.001;
-	//r = height/2* noise(xoff)
-  // Convert polar to cartesian
-
-	worm.display();
-	worm2.display();
-
-  // Increase the angle over time
-}
